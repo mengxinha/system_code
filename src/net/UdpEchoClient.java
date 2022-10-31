357f4e198994c038cdaa9ccfba26099a7be9505a
mengxinha <10169059+mengxinha@user.noreply.gitee.com>
2022-10-31 12:36:40 +0800

    ERROR

--- src/net/UdpEchoClient.java
+++ src/net/UdpEchoClient.java
@@ -1,4 +1,53 @@
 package net;
 
+
+import java.io.IOException;
+import java.net.*;
+
+import java.util.Scanner;
+
 public class UdpEchoClient {
+   private DatagramSocket socket=null;
+   private String serverIP;
+   private int serverPort;
+
+
+
+    public UdpEchoClient(String serverIP,int serverPort) throws SocketException {
+
+        socket = new DatagramSocket();
+        this.serverIP =serverIP;
+        this.serverPort=serverPort;
+    }
+
+
+    public void start() throws IOException {
+        Scanner scanner= new Scanner(System.in);
+        while(true){
+            System.out.println("-> ");
+            String request= scanner.next();
+            DatagramPacket requestPacket = new DatagramPacket(request.getBytes(),request.getBytes().length,
+                    InetAddress.getByName(this.serverIP),this.serverPort
+                    );
+            socket.send(requestPacket);
+
+
+            System.out.println("[%s:%d] req:; resp: \n",requestPacket.getAddress().toString(),requestPacket.getAddress());
+
+
+        }
+
+
+
+    }
+
+    public String process(String request){
+        return request;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        UdpEchoServer server = new UdpEchoServer(9090);
+        server.start();
+    }
 }
--- src/net/UdpEchoServer.java
+++ src/net/UdpEchoServer.java
@@ -1,5 +1,7 @@
 package net;
 
+import javax.swing.plaf.synth.SynthLookAndFeel;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
@@ -12,16 +14,33 @@ public class UdpEchoServer {
 
     }
 
-    public void start(){
+    public void start() throws IOException {
         System.out.println("服务器启动！");
         while (true){
             DatagramPacket requestPacket = new DatagramPacket(new byte[4096], 4096);
-            //socket.receive();
+            socket.receive(requestPacket);
 
             String request = new String(requestPacket.getData(),0,requestPacket.getLength());
 
+            String response = process(request);
 
+            DatagramPacket responsePacket = new DatagramPacket(requestPacket.getData(),0,requestPacket.getLength(),
+            requestPacket.getSocketAddress());
+
+            socket.send(requestPacket);
+
+            System.out.printf("[%s:%d] req: %s; resp:%s \n",requestPacket.getSocketAddress()
+                    .toString(),requestPacket.getPort(),request,response);
         }
+    }
+
+    private String process(String request) {
+        return request;
+    }
+
+    public static void main(String[] args) throws IOException {
+        UdpEchoServer server = new UdpEchoServer(9090);
+        server.start();
     }
 
 
